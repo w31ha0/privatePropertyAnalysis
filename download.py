04a888c4165aedd7fc90db47e3abf58c8e173ced
lewwe <weihaolew93@gmail.com>
2021-06-22 00:14:07 +0800

    Download delta

--- download.py
+++ download.py
@@ -1,17 +1,38 @@
 import requests
-from pandas import json_normalize, to_numeric, set_option, concat, DataFrame
+from pandas import json_normalize, to_numeric, set_option, concat, DataFrame, read_csv
+import pandas as pd
+import json as j
+import os.path
 
 ACCESS_KEY = "13b9f964-e8ae-410a-9447-7742275b517f"
 
+def mergeTransactions(transactions):
+    try:
+        df_transactions = DataFrame()
+        for transaction in transactions:
+            json_str = str(transaction).replace('\'', '"')
+            df_transactions = df_transactions.append(pd.DataFrame.from_dict(j.loads(json_str)))
+        df_transactions = df_transactions.drop_duplicates()
+        df_transactions = df_transactions.fillna(0)
+        dict = list(df_transactions.T.to_dict().values())
+    except Exception as e:
+        print(e)
+    return str(dict)
+
 if __name__ == "__main__":
-    '''response = requests.get(
+    response = requests.get(
             'https://www.ura.gov.sg/uraDataService/insertNewToken.action',
             headers={'AccessKey': ACCESS_KEY, 'User-Agent': None})
-        token = response.json()['Result']
-        print("Got token successful " + token)'''
-    token = "YvvqsJ4UbU4P1k7h5AA3vW-b7-CY4D@R4911ytEck74VvvK86gv2VBhp1PTedj4tD7Rr24x242p4W1Ng2R2u4953h@g-9XMrVb5M"
-    batches = [1, 2, 3, 4]
-    df_overall = DataFrame()
+    token = response.json()['Result']
+    print("Got token successful " + token)
+    #token = "V8p73n-7a6WyK9147p9bP2V24319fq7ZJpF-4+11R1Z-7c9-4e4brnCZ4uynb-1Uv8DBhR6-12+-t9f795fjz6TeGJ7E3f91-8C@"
+    batches = [1]
+    if os.path.isfile('raw.csv'):
+        df_existing = read_csv('raw.csv')
+        df_existing = df_existing.fillna(0)
+    else:
+        df_existing = DataFrame()
+    df_new = DataFrame()
     for batch in batches:
         print("Fetching batch " + str(batch))
         response = requests.get(
@@ -19,6 +40,10 @@ if __name__ == "__main__":
             headers={'AccessKey': ACCESS_KEY, 'Token': token, 'User-Agent': None})
         properties = response.json()['Result']
         df_properties = json_normalize(properties)
-        df_overall = concat([df_overall, df_properties])
+        df_new = concat([df_new, df_properties])
 
-    df_overall.to_csv("raw.csv")
+    df_combined = df_existing.append(df_new).groupby(['project', 'street'], as_index=False).agg({'transaction': lambda x:mergeTransactions(x),
+                                                                                                'street':'first', 'project':'first', 'marketSegment':'first',
+                                                                                                'x': 'first', 'y': 'first'})
+    diff = pd.concat([df_combined, df_existing]).drop_duplicates(keep=False)
+    df_combined.to_csv("raw.csv", mode='w+')
