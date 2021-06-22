34e67eca9f551130f0be153eff63c32cd31395f5
lewwe <weihaolew93@gmail.com>
2021-06-22 20:49:55 +0800

    Fix bugs

--- download.py
+++ download.py
@@ -16,18 +16,28 @@ def mergeTransactions(transactions):
             df_transactions = df_transactions.append(df_transaction)
         df_transactions = df_transactions.drop_duplicates()
         df_transactions.sort_values(df_transactions.columns.to_list())
-        dict = list(df_transactions.T.to_dict().values())
+        dict = list(df_transactions.reset_index(drop=True).T.to_dict().values())
     except Exception as e:
         print(e)
     return str(dict)
 
+def diffTransactions(transactions):
+    df_transactions = DataFrame()
+    for transaction in transactions:
+        json_str = str(transaction).replace('\'', '"')
+        df_transaction = pd.DataFrame.from_dict(j.loads(json_str))
+        df_transaction = df_transaction.fillna(0)
+        df_transactions = df_transactions.append(df_transaction)
+    df_transactions = df_transactions.drop_duplicates(keep=False)
+    return str(list(df_transactions.reset_index(drop=True).T.to_dict().values()))
+
 if __name__ == "__main__":
-    response = requests.get(
+    '''response = requests.get(
             'https://www.ura.gov.sg/uraDataService/insertNewToken.action',
             headers={'AccessKey': ACCESS_KEY, 'User-Agent': None})
     token = response.json()['Result']
-    print("Got token successful " + token)
-    #token = "V8p73n-7a6WyK9147p9bP2V24319fq7ZJpF-4+11R1Z-7c9-4e4brnCZ4uynb-1Uv8DBhR6-12+-t9f795fjz6TeGJ7E3f91-8C@"
+    print("Got token successful " + token)'''
+    token = "7RA5fRp7pwH1C74-5F2dbF2f4SZQ79bY+z27sfm4c-71a4gtkeJ@Q5P39b74-7P5ef9--JuWjs7ze4cvA42EaJ9tRaFvn74FF7Ta"
     batches = [1, 2, 3, 4]
     if os.path.isfile('raw.csv'):
         df_existing = read_csv('raw.csv')
@@ -48,6 +58,7 @@ if __name__ == "__main__":
                                                                                                 'street':'first', 'project':'first', 'marketSegment':'first',
                                                                                                 'x': 'first', 'y': 'first'})
 
+    print("")
     diff = pd.concat([df_combined, df_existing]).drop_duplicates(subset=['street', 'project'], keep=False)
     if not diff.empty:
         print("New Residences:")
