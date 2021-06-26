90241c8b8819b5f6982050dc4eb17c1f891c8131
lewwe <weihaolew93@gmail.com>
2021-06-26 10:53:55 +0800

    Add Distance to central + logging

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pandas import set_option
 import pandas as pd
 import json as j
 import math
+import logging as log
 
 ACCESS_KEY = "13b9f964-e8ae-410a-9447-7742275b517f"
 
@@ -58,17 +59,38 @@ def leaseCommencement(tenure):
     else:
         return 0
 
+def distanceToCentral(x, y):
+    if x == 0.0 or y == 0.0:
+        return math.inf
+
+    central_coordinates = [29365.079, 31281.379]
+    return math.sqrt((central_coordinates[0] - x)**2 + (central_coordinates[1] - y)**2)
+
 if __name__ == "__main__":
+    log.basicConfig(level=log.INFO, format='%(asctime)s.%(msecs)03d %(levelname)s - %(funcName)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
     df_properties = pd.read_csv('raw.csv')
+    log.info("Loaded into csv")
     df_properties['annual_appreciation'] = df_properties['transaction'].apply(lambda x: calculate_annual_appreciation(x))
+    log.info("Done calculating annual appreciation")
     df_properties['enBlock'] = df_properties['transaction'].apply(lambda x: en_block(x))
+    log.info("Done calculating enBlock")
     df_properties['propertyType'] = df_properties['transaction'].apply(lambda x: flatten(x, 'propertyType'))
+    log.info("Done calculating propertyType")
     df_properties['initialYear'] = df_properties['transaction'].apply(lambda x: yearBegan(x))
+    log.info("Done calculating initial Year")
     df_properties['leaseCommencement'] = df_properties['transaction'].apply(lambda x: leaseCommencement(flatten(x, "tenure")))
+    log.info("Done calculating lease commencement")
     df_properties['totalLease'] = df_properties['transaction'].apply(lambda x: totalLeaseYears(flatten(x, 'tenure')))
+    log.info("Done calculating total lease")
     df_properties['leaseRemaining'] = df_properties['totalLease'] - (df_properties['initialYear'] - df_properties['leaseCommencement'])
+    log.info("Done calculating lease remaining")
     df_properties['tenure'] = df_properties['transaction'].apply(lambda x: compressTenure(flatten(x, 'tenure')))
+    log.info("Done calculating tenure")
     df_properties['noOfTransactions'] = df_properties['transaction'].apply(lambda x: len(x))
+    log.info("Done calculating number of transactions")
+    df_properties['distanceToCentral'] = df_properties.apply(lambda df: distanceToCentral(df['x'], df['y']), axis=1)
+    log.info("Done calculating distance to central")
     df_properties = df_properties.dropna()
 
     appreciation_threshold = 0.03
@@ -84,4 +106,4 @@ if __name__ == "__main__":
     set_option("display.max_rows", None)
     set_option('display.max_columns', 5)
     df_properties.sort_values('annual_appreciation', ascending=False, inplace=True)
-    df_properties[['street', 'project', 'marketSegment', 'propertyType', 'enBlock', 'tenure', 'annual_appreciation', 'noOfTransactions', 'initialYear', 'leaseCommencement', 'leaseRemaining']].to_csv('Analysis.csv')
+    df_properties[['street', 'project', 'marketSegment', 'propertyType', 'enBlock', 'tenure', 'annual_appreciation', 'noOfTransactions', 'initialYear', 'leaseCommencement', 'leaseRemaining', 'distanceToCentral']].to_csv('Analysis.csv')
